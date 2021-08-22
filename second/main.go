e15228938e9b86424d0e9f1adc63ae102151e4e9
bujosa <davidbujosa@gmail.com>
2021-08-22 03:36:06 -0400

    Finished second crawl test url

--- second/main.go
+++ second/main.go
@@ -1,4 +1,15 @@
-package main 
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+// Site to crawl
+const site = "https://en.wikipedia.org/wiki/Bad_Bunny"
+
 type wikipedia struct {
 	Title      string
 	Picture    string
@@ -8,5 +19,33 @@ type wikipedia struct {
 }
 
 func main() {
+	c := colly.NewCollector()
 
+    page := wikipedia{}
+
+	c.OnHTML(".infobox", func(e *colly.HTMLElement) {
+		
+		page.Title = e.ChildText("#mw-content-text > div.mw-parser-output > table.infobox.biography.vcard > tbody > tr:nth-child(1) > th > div")
+		page.Picture = e.ChildAttr("#mw-content-text > div.mw-parser-output > table.infobox.biography.vcard > tbody > tr:nth-child(2) > td > a > img", "src")
+		page.Born = e.ChildText("#mw-content-text > div.mw-parser-output > table.infobox.biography.vcard > tbody > tr:nth-child(3) > td")
+		page.Occupation = e.ChildText("#mw-content-text > div.mw-parser-output > table.infobox.biography.vcard > tbody > tr:nth-child(4) > td")
+		page.Genres = e.ChildText("#mw-content-text > div.mw-parser-output > table.infobox.biography.vcard > tbody > tr:nth-child(8) > td > div > ul")
+
+	})
+
+	err := c.Visit(site)
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.Wait()
+
+	bs, err := json.MarshalIndent(page, "", "\t")
+	
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bs))
 }
